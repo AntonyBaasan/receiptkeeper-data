65136a6d4443f24fcf47bb3ff8610717479dd97e
Ankhbayar <antony.baasan@gmail.com>
2018-06-06 14:00:59 -0400

    add findByOwner

--- src/main/java/com/antonybaasan/receiptkeeper/restdata/controllers/ReceiptController.java
+++ src/main/java/com/antonybaasan/receiptkeeper/restdata/controllers/ReceiptController.java
@@ -2,6 +2,7 @@ package com.antonybaasan.receiptkeeper.restdata.controllers;
 
 import com.antonybaasan.receiptkeeper.restdata.domains.Receipt;
 import com.antonybaasan.receiptkeeper.restdata.repositories.ReceiptRepository;
+import com.antonybaasan.receiptkeeper.restdata.security.AuthFacade;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,19 @@ import java.util.List;
 @RestController
 public class ReceiptController {
     private ReceiptRepository repository;
+    private AuthFacade auth;
 
-    public ReceiptController(ReceiptRepository repository) {
+    public ReceiptController(ReceiptRepository repository, AuthFacade auth) {
         this.repository = repository;
+        this.auth = auth;
     }
 
     @RequestMapping("/r")
     public Object getReceipts() {
 
-        Object all = this.repository.findAll();
+        String ownerId = auth.getUser().getUid();
+//        Object all = this.repository.findAll();
+        Object all = this.repository.findByOwner(ownerId);
         return all;
     }
 
--- src/main/java/com/antonybaasan/receiptkeeper/restdata/repositories/ReceiptRepository.java
+++ src/main/java/com/antonybaasan/receiptkeeper/restdata/repositories/ReceiptRepository.java
@@ -3,6 +3,8 @@ package com.antonybaasan.receiptkeeper.restdata.repositories;
 import com.antonybaasan.receiptkeeper.restdata.domains.Receipt;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-//@RepositoryRestResource(collectionResourceRel = "receipts", path = "receipts")
+import java.util.List;
+
 public interface ReceiptRepository extends PagingAndSortingRepository<Receipt, Long> {
+    List<Receipt> findByOwner(String ownerId);
 }
