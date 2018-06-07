985d9c5ae48a069a900f1ba218232a84321639d5
Antony Baasandorj <antony.baasan@gmail.com>
2018-06-07 14:50:13 -0400

    read firebase admin key file from differen sources

--- src/main/java/com/antonybaasan/receiptkeeper/restdata/config/MainConfig.java
+++ src/main/java/com/antonybaasan/receiptkeeper/restdata/config/MainConfig.java
@@ -3,6 +3,7 @@ package com.antonybaasan.receiptkeeper.restdata.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -16,7 +17,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.DirectoryNotEmptyException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 
 @Configuration
 public class MainConfig extends ResourceServerConfigurerAdapter {
@@ -24,8 +30,11 @@ public class MainConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private ResourceLoader resourceLoader;
 
-    @Value("${firebase.secretfile.path}")
-    private String firebaseSecretfilePath;
+    @Value("${firebase.secretfile.class-path}")
+    private String firebaseSecretfileClassPath;
+
+    @Value("${firebase.secretfile.file-path}")
+    private String firebaseSecretfileFilePath;
 
     @Value("${firebase.database.url}")
     private String firebaseDatabaseUrl;
@@ -33,7 +42,7 @@ public class MainConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public FirebaseApp firebaseApp() {
 
-        Resource resource = resourceLoader.getResource(this.firebaseSecretfilePath);
+        Resource resource = getResource();
 
         FirebaseOptions options;
         try {
@@ -44,6 +53,45 @@ public class MainConfig extends ResourceServerConfigurerAdapter {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private Resource getResource() {
+
+        Resource resource = resourceLoader.getResource("classpath:" + this.firebaseSecretfileClassPath);
+        if (resource.exists()) {
+            return resource;
+        }
+
+        this.createFribaseFileIfNotExists(this.firebaseSecretfileFilePath);
+        return resourceLoader.getResource("file:" + this.firebaseSecretfileFilePath);
+    }
+
+    private void createFribaseFileIfNotExists(String fileName) {
+//        String fileName = "C:/firebase-adminsdk2.json";
+
+        deleteFile(fileName);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("website", "www.websparrow.org2222");
+
+        try (FileWriter file = new FileWriter(fileName)) {
+            file.write(jsonObject.toString());
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteFile(String fileName) {
+        try {
+            Files.deleteIfExists(Paths.get(fileName));
+        } catch (NoSuchFileException e) {
+            System.out.println("No such file/directory exists");
+        } catch (DirectoryNotEmptyException e) {
+            System.out.println("Directory is not empty.");
+        } catch (IOException e) {
+            System.out.println("Invalid permissions.");
+        }
     }
 
     @Bean
