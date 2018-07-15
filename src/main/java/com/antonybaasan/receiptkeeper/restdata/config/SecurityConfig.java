ae5e846c7da4251bdb65f74b7862e8276c839be0
Antony Baasandorj <antony.baasan@gmail.com>
2018-07-14 23:03:29 -0400

    add missing ref

--- src/main/java/com/antonybaasan/receiptkeeper/restdata/config/SecurityConfig.java
+++ src/main/java/com/antonybaasan/receiptkeeper/restdata/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.antonybaasan.receiptkeeper.restdata.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
