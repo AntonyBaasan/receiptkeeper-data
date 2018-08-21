4d3987dd587f38a6f911275a37cf1afd4fbe4148
Antony Baasandorj <antony.baasan@gmail.com>
2018-08-21 15:46:48 -0400

    add record and user models

---
+++ src/main/java/com/antonybaasan/receiptkeeper/restdata/domains/Record.java
@@ -0,0 +1,101 @@
+package com.antonybaasan.receiptkeeper.restdata.domains;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Record {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String title;
+    private String description;
+    private String imageUrl;
+    private Date date;
+    private Date insertedDate;
+    private double total;
+    private String[] tags;
+    @ManyToOne
+    private User owner;
+
+    public Record() {
+    }
+
+    public Record(long id, User owner){
+        this.id = id;
+        this.owner = owner;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getInsertedDate() {
+        return insertedDate;
+    }
+
+    public void setInsertedDate(Date insertedDate) {
+        this.insertedDate = insertedDate;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public String[] getTags() {
+        return tags;
+    }
+
+    public void setTags(String[] tags) {
+        this.tags = tags;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
+
---
+++ src/main/java/com/antonybaasan/receiptkeeper/restdata/domains/User.java
@@ -0,0 +1,78 @@
+package com.antonybaasan.receiptkeeper.restdata.domains;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String imageUrl;
+    private Date startDate;
+    @OneToMany(mappedBy = "owner")
+    private Set<Record> records;
+
+    public User() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Set<Record> getRecords() {
+        return records;
+    }
+
+    public void setRecords(Set<Record> records) {
+        this.records = records;
+    }
+}
