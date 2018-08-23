e6dbce0419d13cdcf976420fd434c84662522151
Antony Baasandorj <antony.baasan@gmail.com>
2018-08-23 10:30:37 -0400

    create User controller. #19

---
+++ src/main/java/com/antonybaasan/receiptkeeper/restdata/controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.antonybaasan.receiptkeeper.restdata.controllers;
+
+import com.antonybaasan.receiptkeeper.restdata.model.User;
+import com.antonybaasan.receiptkeeper.restdata.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public ResponseEntity<User> delete(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(userService.getUserById(id), HttpStatus.OK);
+    }
+
+}
