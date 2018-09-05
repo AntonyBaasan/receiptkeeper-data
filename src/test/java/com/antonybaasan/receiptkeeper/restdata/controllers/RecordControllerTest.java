c337848f20362f8c0a124f531856946645911061
Antony Baasandorj <antony.baasan@gmail.com>
2018-09-05 09:44:39 -0400

    add ut case

--- src/test/java/com/antonybaasan/receiptkeeper/restdata/controllers/RecordControllerTest.java
+++ src/test/java/com/antonybaasan/receiptkeeper/restdata/controllers/RecordControllerTest.java
@@ -2,6 +2,8 @@ package com.antonybaasan.receiptkeeper.restdata.controllers;
 
 import com.antonybaasan.receiptkeeper.restdata.model.Record;
 import com.antonybaasan.receiptkeeper.restdata.service.RecordService;
+import com.google.firebase.auth.FirebaseAuthException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,19 +35,35 @@ public class RecordControllerTest {
     @Autowired
     public MockMvc mockMvc;
 
+    private String accessToken;
+
+    @Before
+    public void beforeMethods() throws FirebaseAuthException {
+        if(accessToken == null) {
+            accessToken = generateToken(firebasePrivateKey, "1");
+        }
+    }
+
     @Test
     public void getRecordById_Success() throws Exception {
-
         Record record = createRecord(1);
         when(recordService.getRecord(1)).thenReturn(record);
-
-        String accessToken = generateToken(firebasePrivateKey, "1");
 
         mockMvc.perform(get("/records/1")
                 .header("Authorization", "Bearer " + accessToken))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(content().json("{}"));
+    }
+
+    @Test
+    public void getRecordById_WhenNoRecord_ReturnEmptyBody() throws Exception {
+        when(recordService.getRecord(2)).thenReturn(null);
+
+        mockMvc.perform(get("/records/2")
+                .header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
     }
 
 }
